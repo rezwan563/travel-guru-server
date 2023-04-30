54db9b3e1f79f86f99f2efaec285272b04dc3459
Rezwan <t.rewan17@gmail.com>
2023-04-30 12:11:49 +0600

    Added destination data

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require('cors');
 const port = 5000;
 
+const destinations = require('./data/destination.json')
+
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send("hello  world from server")
+})
+
+app.get('/destinations', (req, res) =>{
+    res.send(destinations);
 })
 
 app.listen(port, () =>{
